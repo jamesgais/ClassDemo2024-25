9ca6c2ef0aba54e9b1cc5e0185fc4010f5ac3ab6
jamesgais <gais.james@bphawks.org>
2024-09-17 13:57:51 -0400

    9.17 notes

--- variblesPrintInput.py
+++ variblesPrintInput.py
@@ -1,7 +1,28 @@
 import os
 os.system("cls")
+# this is a comment
 
-print("Hello World")
+name = "Bob"
+number = 23
+floating_number = 23.2345
+
+print(name,number,floating_number)
+
+#persons_name = input("Please Enter your name:")
+#print(persons_name)
+
+print('Hello world')
+print("Hello world")
+print('''Hello World''')
+print("""Hello World""")
+
+print("Then he said 'eat'. ")
 
 
-#%asdfasdfasdfasjhljhlkjhs
+print("""line1
+      line2
+      line3""")
+
+
+
+
