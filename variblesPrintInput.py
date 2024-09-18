6bcc38f9f5d214ab754e596a6b7b5b0af6da5215
jamesgais <gais.james@bphawks.org>
2024-09-18 13:15:55 -0400

    9-18 notes

--- variblesPrintInput.py
+++ variblesPrintInput.py
@@ -8,20 +8,35 @@ floating_number = 23.2345
 
 print(name,number,floating_number)
 
-#persons_name = input("Please Enter your name:")
-#print(persons_name)
+persons_name = input("Please Enter your name:\t")
+print(persons_name)
 
 print('Hello world')
 print("Hello world")
 print('''Hello World''')
 print("""Hello World""")
 
-print("Then he said 'eat'. ")
-
+print('''Then he said "eat". ''')
 
 print("""line1
       line2
       line3""")
+
+
+firstName = "Jim"
+lastName = "Bob"
+age = 45
+
+print(firstName, lastName,age, sep='\t')
+print("Samant", "Brown", 56, sep ='\t')
+
+print("\n\n")
+
+print(firstName, end="-->")
+print(lastName, end="-->")
+print(age, end="-->")
+
+print("\n\n")
 
 
 
