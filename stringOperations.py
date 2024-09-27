ad74c31f9d0ca11a5dfd587d4c83ac6197a16933
jamesgais <gais.james@bphawks.org>
2024-09-27 13:38:09 -0400

    string notes

---
+++ stringOperations.py
@@ -0,0 +1,19 @@
+
+import os
+os.system('cls')
+
+saying = "do or do not, there is no try."
+print(saying.replace(" ",""))
+
+print(len(saying))
+
+print(saying)
+print(len(saying))
+print(saying[30:0:-5])#goes backwards   
+print(saying[-30])
+
+print()
+name = "\t" + input("Enter your first name")
+print(name)
+print(name.lstrip("\t"))
+print("thing")
