b56d067808f660440032131decdb0e3eb57a7f33
jamesgais <gais.james@bphawks.org>
2024-10-24 12:54:31 -0400

    l

---
+++ if blocks.py
@@ -0,0 +1,38 @@
+
+var = '9'
+
+if '9' not in var:
+    print("its is there")
+else: 
+    print("its not in there")
+
+
+if var is "9":
+    print("Hellow")
+
+
+
+
+
+
+name = "bo"
+
+
+if name == "bob": 
+    print(f'Hello {name}')
+elif name == "suzy": 
+    print(f'Welcome{name}')
+elif name == "jimmy": 
+    print(f"Good day")
+else: 
+    print("go away")
+
+score = 90
+
+if score >= 90 and score <=100:
+    grade = 'A'
+elif score >= 80 and score < 90:
+    grade = 'B'
+
+
+    
